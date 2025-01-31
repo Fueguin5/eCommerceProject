e64b17e9d08ed5b63f9ed621733439e45157fb3c
Grant <grant.leatherman05@gmail.com>
2025-01-31 11:57:46 -0500

    Add project files.

---
+++ Library.eCommerce/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommercePlatform.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Display
+        {
+            get
+            {
+                return $"{Id}. {Name}";
+            }
+        }
+        public Product()
+        {
+            Name = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return Display ?? string.Empty;
+        }
+    }
+}
---
+++ eCommerceProject/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using eCommercePlatform.Models;
+using Library.eCommerce.Services;
+
+namespace ECommerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lastKey = 0;
+            Console.WriteLine("Welcome to Congo!");
+            Console.WriteLine("C. Create new inventory item");
+            Console.WriteLine("R. Read all inventory items");
+            Console.WriteLine("U. Update an inventory item");
+            Console.WriteLine("D. Delete an inventory item");
+            Console.WriteLine("Q. Quit");
+
+            List<Product?> list = ProductServiceProxy.Current.Products;
+
+            char choice = 'Z';
+            do
+            {
+                string? input = Console.ReadLine();
+                choice = input[0];
+                switch (choice)
+                {
+                    case 'C':
+                    case 'c':
+                        list.Add(new Product
+                        {
+                            Id = ++lastKey,
+                            Name = Console.ReadLine()
+                        });
+                        break;
+                    case 'R':
+                    case 'r':
+                        list.ForEach(Console.WriteLine);
+                        break;
+                    case 'U':
+                    case 'u':
+                        //select one product and replace with another
+                        Console.WriteLine("Which product would you like to update?");
+                        int selection = int.Parse(Console.ReadLine() ?? "-1");
+                        var selectedProd = list.FirstOrDefault(p => p.Id == selection);
+                        if (selectedProd != null)
+                        {
+                            selectedProd.Name = Console.ReadLine() ?? "ERROR";
+                        }
+                        break;
+                    case 'D':
+                    case 'd':
+                        //select a product and delete it
+                        Console.WriteLine("Which product would you like to delete?");
+                        selection = int.Parse(Console.ReadLine() ?? "-1");
+                        selectedProd = list.FirstOrDefault(p => p.Id == selection);
+                        list.Remove(selectedProd);
+                        break;
+                    case 'Q':
+                    case 'q':
+                        break;
+                    default:
+                        Console.WriteLine("Error: Unknown Command");
+                        break;
+                }
+            } while (choice != 'Q' && choice != 'q');
+
+            Console.ReadLine();
+        }
+    }
+}
