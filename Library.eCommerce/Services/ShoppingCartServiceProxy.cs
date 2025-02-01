277d1b2b96244847448cd3b2ab6462cdd8656e8c
Grant <grant.leatherman05@gmail.com>
2025-01-31 19:39:13 -0500

    added MAUI

---
+++ Library.eCommerce/Services/ShoppingCartServiceProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.eCommerce.Services
+{
+    internal class ShoppingCartServiceProxy
+    {
+        //copy and paste the ProductServiceProxy code and then change it to be a cart
+    }
+}
